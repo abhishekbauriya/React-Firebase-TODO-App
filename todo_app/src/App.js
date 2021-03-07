63beb596773259d0c83ae2425faa3da77237b36a
abhishekbauriya <abhishekbauriya007@gmail.com>
2021-03-07 15:42:09 +0530

    Added Material-UI

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out', 'You can add one by one']);
+  const [input, setInput] = useState('');
+  console.log('🌟', input);
+
+  const addTodo = (event) => {
+    // this will fire off when we click the button
+    event.preventDefault();
+    // this will stop the REFRESH
+    console.log('🚀', 'Im working !!!');
+    setTodos([...todos, input]);
+    setInput(''); // clear up the input after clicking add todo button
+    console.log(todos);
+  }
+
   return (
     <div className="App">
-      <h1>HELLO WORLD !!!</h1>
+      <h1>HELLO WORLD Programmers !!!</h1>
+      
+      <form>
+
+        <FormControl>
+          <InputLabel>✅ Write a Todo</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+        </FormControl>
+
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+          </Button>
+        {/* <button type="submit" onClick={addTodo}>Add Todo</button> */}
+      </form>
+      
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+
+
     </div>
   );
 }
